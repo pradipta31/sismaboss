3b225dfe02fb6ef5ad7e048015cd722d477387a6
pradipta31 <pradiptadipta31@gmail.com>
2019-01-13 13:12:32 +0800

    update icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SISMA UKM BOSS</title>
+  <title>SISMABOSS</title>
+  <link rel="icon" href="images/logo_boss.png">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
